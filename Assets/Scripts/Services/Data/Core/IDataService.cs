59b467d8d6c0754d1abed8cca07a428e5c8d6bf7
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-09-11 19:56:12 +0300

    Added DataService;

---
+++ Assets/Scripts/Services/Data/Core/IDataService.cs
@@ -0,0 +1,13 @@
+﻿namespace Services.Data.Core
+{
+    public interface IDataService
+    {
+        public void Save<T>(T data, string key);
+
+        public T Load<T>(string key);
+
+        public T Load<T>(string key, T defaultValue);
+
+        public bool HasKey(string key);
+    }
+}
