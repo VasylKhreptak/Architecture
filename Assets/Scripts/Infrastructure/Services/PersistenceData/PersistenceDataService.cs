ef6fa73e3b7cb3c146ba037e9a128f177ffa3613
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-09-12 17:59:16 +0300

    Updated PersistenceDataService;

--- Assets/Scripts/Infrastructure/Services/PersistenceData/PersistenceDataService.cs
+++ Assets/Scripts/Infrastructure/Services/PersistenceData/PersistenceDataService.cs
@@ -1,21 +1,32 @@
-using Infrastructure.Data;
 using Infrastructure.Data.Persistence;
 using Infrastructure.Services.PersistenceData.Core;
+using Infrastructure.Services.SaveLoad.Core;
+using Zenject;
 
 namespace Infrastructure.Services.PersistenceData
 {
     public class PersistenceDataService : IPersistenceDataService
     {
+        private const string PlayerDataKey = "PlayerData";
+
+        private ISaveLoadService _saveLoadService;
+
+        [Inject]
+        private void Constructor(ISaveLoadService saveLoadService)
+        {
+            _saveLoadService = saveLoadService;
+        }
+
         public PlayerData PlayerData { get; set; }
-        
+
         public void Save()
         {
-            
+            _saveLoadService.Save(PlayerDataKey, PlayerDataKey);
         }
-        
+
         public void Load()
         {
-            
+            PlayerData = _saveLoadService.Load(PlayerDataKey, new PlayerData());
         }
     }
 }
