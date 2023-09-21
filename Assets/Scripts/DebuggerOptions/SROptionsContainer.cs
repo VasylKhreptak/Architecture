d5826d4eb3b5df72ab85f155aec1e87a2d38b90f
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-09-21 09:52:46 +0300

    Updated SROptionsContainer;

--- Assets/Scripts/DebuggerOptions/SROptionsContainer.cs
+++ Assets/Scripts/DebuggerOptions/SROptionsContainer.cs
@@ -1,11 +1,20 @@
-﻿using System.ComponentModel;
+﻿using Infrastructure.StateMachine.Game.States;
+using Infrastructure.StateMachine.Main.Core;
+using Infrastructure.StateMachine.Main.States.Core;
 using JetBrains.Annotations;
 
 namespace DebuggerOptions
 {
     public class SROptionsContainer
     {
-        [Category("Main Category"), UsedImplicitly]
-        public void PrintMessageToConsole() => UnityEngine.Debug.Log("SROptions work's perfectly!");
+        private readonly IStateMachine<IGameState> _stateMachine;
+
+        public SROptionsContainer(IStateMachine<IGameState> stateMachine)
+        {
+            _stateMachine = stateMachine;
+        }
+
+        [UsedImplicitly] 
+        public void EnterBootstrapState() => _stateMachine.Enter<BootstrapState>();
     }
 }
