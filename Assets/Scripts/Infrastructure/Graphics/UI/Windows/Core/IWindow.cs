6507840c62a8cf44cbcecaf546d721d0496bc57d
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-10-04 20:31:14 +0300

    Added IWindow Interface;

---
+++ Assets/Scripts/Infrastructure/Graphics/UI/Windows/Core/IWindow.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Graphics.UI.Windows.Core
+{
+    public interface IWindow
+    {
+        public bool Enabled { get; }
+
+        public void Show();
+
+        public void Hide();
+    }
+}
