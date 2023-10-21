a10a938bf1868a31835a04b5af55b75d1946d2ef
VASYL\chrep <vasylkhreptak12@gmail.com>
2023-10-21 14:45:54 +0300

    Added GamePrefabs;

---
+++ Assets/Scripts/Infrastructure/Data/Static/GamePrefabs.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Infrastructure.Data.Static
+{
+    [CreateAssetMenu(fileName = "GamePrefabs", menuName = "ScriptableObjects/Static/GamePrefabs", order = 0)]
+    public class GamePrefabs : ScriptableObject
+    {
+        
+    }
+}
