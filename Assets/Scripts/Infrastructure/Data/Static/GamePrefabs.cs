de81cd5a0293738c5c8aef37afb6f36f24ff84c3
VASYL\chrep <vasylkhreptak12@gmail.com>
2023-11-17 11:55:27 +0200

    Updated Game Prefabs;

--- Assets/Scripts/Infrastructure/Data/Static/GamePrefabs.cs
+++ Assets/Scripts/Infrastructure/Data/Static/GamePrefabs.cs
@@ -8,7 +8,7 @@ namespace Infrastructure.Data.Static
     [CreateAssetMenu(fileName = "GamePrefabs", menuName = "ScriptableObjects/Static/GamePrefabs", order = 0)]
     public class GamePrefabs : ScriptableObject
     {
-        [OdinSerialize] private Dictionary<Prefab, GameObject> _prefabs;
+       [SerializeField] private SerializedDictionary<Prefab, GameObject> _prefabs = new SerializedDictionary<Prefab, GameObject>();
 
         public GameObject this[Prefab prefab] => _prefabs[prefab];
     }
