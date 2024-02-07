ec2f6149ed2a64478ec38e5c316f1c2e0c46b12f
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-07 09:16:41 +0200

    - refactored IWindow;

--- Assets/Scripts/Infrastructure/Graphics/UI/Windows/Core/IWindow.cs
+++ Assets/Scripts/Infrastructure/Graphics/UI/Windows/Core/IWindow.cs
@@ -4,8 +4,6 @@ namespace Infrastructure.Graphics.UI.Windows.Core
 {
     public interface IWindow
     {
-        public IReadOnlyReactiveProperty<bool> IsActive { get; }
-
         public void Show();
 
         public void Hide();
